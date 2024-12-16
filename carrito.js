06d1c57509843abb8dafeec8a4443d98788a9df6
EvangelinaGarcia <evangelinagarcia3@hotmail.com>
2024-12-16 10:27:09 -0300

    Add files via upload

--- carrito.js
+++ carrito.js
@@ -3,6 +3,7 @@ const carrito = JSON.parse (localStorage.getItem("carrito")) || [];
 const mostrarCarrito = ()=>{
     const lista = document.getElementById ("lista-carrito")
     lista.innerHTML = "";
+
     if (carrito.length===0){
         lista.innerHTML = '<p> Tu Carrito esta vacio</p>'
         return;
@@ -17,11 +18,27 @@ const mostrarCarrito = ()=>{
         `;
         lista.appendChild (producto)
 
-    })
+    });
+
+    actualizarResumen()
 
 
 
 }
+
+const actualizarResumen = () => {
+    const totalProductos = document.getElementById("total-productos");
+    const importeTotal = document.getElementById("importe-total");
+
+    const total = carrito.reduce((acc, item) => acc + item.precio, 0);
+    totalProductos.textContent = carrito.length;
+    importeTotal.textContent = total.toFixed(2);
+
+    const botonCompra = document.querySelector("button[onclick='realizarCompra()']");
+    const resumenCarrito = document.getElementById("resumen-carrito");
+    resumenCarrito.appendChild(botonCompra);
+};
+
 const EliminarDelCarrito =(indice)=>{
     carrito.splice (indice,1);
     localStorage.setItem ("carrito",JSON.stringify(carrito))
